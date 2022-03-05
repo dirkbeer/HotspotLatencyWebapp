5defbb3088a79b6e5c9ea10a3b01f40a8c701241
Dirk Beer <rdbeer@pm.me>
2022-03-05 12:37:29 -0800

    R scripts from HotspotLatency adapted to shiny, with general improvements

---
+++ app.R
@@ -0,0 +1,40 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+source("plotHotspotLatencies.R")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Hotspot Latency"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(position = "right",
+        sidebarPanel(width=0),
+        mainPanel(
+           plotOutput("latencyPlot"),
+           width=12
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$latencyPlot <- renderPlot({
+
+        plotHotspotLatencies()
+        
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ get_helium_latency.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 
+# Add the following line to your crontab using crontab -e:
+# 
+#   */10 * * * * /home/myusername/get_helium_latency.sh
+#
+
+outfilename=hotspot_latency.txt
+datetime=$(date | sed 's/\n//')
+
+function get_hotspot_data () {
+        hs=("$@")
+        result=$(nmap -oG /dev/stdout -Pn -n -p ${hs[2]} -sV ${hs[1]} | grep Ports | sed 's/^.*Ports: //' | sed 's/Host is up\.//' |  sed 's/Host is up (//' | sed 's/s latency)\.//' | sed 's:/:\t:g')
+        printf "$datetime\t${hs[0]}\t${hs[1]}\t$result\n" >> $outfilename
+}
+
+hs=(tadpole 68.8.112.183 44158)
+get_hotspot_data "${hs[@]}"
+
+hs=(buffalo 99.171.178.180 44158)
+get_hotspot_data "${hs[@]}"
+
+hs=(beaver 72.26.117.34 44158)
+get_hotspot_data "${hs[@]}"
+
+hs=(spider 72.26.117.34 44158)
+get_hotspot_data "${hs[@]}"
+
+# keep file size reasonable by only keeping a maximum number of lines
+echo "$(tail -1008 $outfilename)" > $outfilename
